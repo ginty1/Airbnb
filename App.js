5a36ea1227e8f22261ae9202aa817579d71d68a4
ginty1 <chiombagintykondwani@gmail.com>
2021-01-08 01:36:04 +0200

    project setup/configuration

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Feather from 'react-native-vector-icons/Fontisto';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Feather name="blind"/>
       <StatusBar style="auto" />
     </View>
   );
