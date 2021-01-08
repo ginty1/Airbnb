65117001488b2739aa484cdf86e119c07245ab67
ginty1 <chiombagintykondwani@gmail.com>
2021-01-08 22:01:56 +0200

    render post

--- scr/components/Post/index.js
+++ scr/components/Post/index.js
@@ -12,12 +12,21 @@ const Post=(props)=>{
 
 
           {/* bed and bedroom */}
-
-
-          {/* old price & new price */}
-
+             <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+             <Text style={styles.description}
+             numberOfLines={3}>Suppose that you are a young child who lives in a small rural town. Every day as you go to school,
+                you watch as the buses pass. During summer break,
+                 you visit your aunt who lives in a major city.
+                  She takes you on a trip to the local railroad station so that you can watch the trains come and go.</Text> 
+         {/* old price & new price */}
+          <Text style={styles.prices}>
+            <Text style={styles.oldPrice}>k250</Text>
+            <Text style={styles.newPrice}> k200</Text>
+            / night
+          </Text>
 
           {/*  Total price */}
+          <Text style={styles.totalPrice}>k450 total</Text>
         </View>
     )
 }
--- scr/components/Post/styles.js
+++ scr/components/Post/styles.js
@@ -8,7 +8,30 @@ const styles = StyleSheet.create({
         aspectRatio:3/2,
         resizeMode:'cover',
         borderRadius:15,
-    }
-    
-})
+    },
+    bedrooms:{
+        marginVertical:8,
+        color:'#000',
+        fontSize:18,
+
+    },
+    decription:{
+        fontSize:18,
+        lineHeight:26,
+
+    },
+    prices:{
+        fontSize:18,
+        marginVertical:10,
+    },
+    oldPrice:{
+        color:'#5b5b5b',
+        textDecorationLine:'line-through',},
+    newPrice:{
+        fontWeight:'bold',
+        },
+    totalPrice:{
+        color:'#5b5b5b',
+        textDecorationLine:'underline',}
+    })
 export default styles;
