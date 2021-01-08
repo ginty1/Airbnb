aff7780b620ffdc163e8a6aa44801895408818a1
ginty1 <chiombagintykondwani@gmail.com>
2021-01-09 00:12:08 +0200

    post props

--- App.js
+++ App.js
@@ -3,22 +3,24 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import HomeScreen from './scr/screens/Home'
 import Post from './scr/components/Post'
+import feed from './assets/AirbnbAssets/feed'
 
-
+const post1=feed[0]
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Post post={post1}/>
+    
       {/* <HomeScreen/> */}
-      <Post/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  flex: 1,
   container: {
-    flex: 1,
     backgroundColor: '#fff',
   },
 });
--- scr/components/Post/index.js
+++ scr/components/Post/index.js
@@ -3,30 +3,34 @@ import {View,Text, Image}  from 'react-native'
 import styles from './styles'
 
 const Post=(props)=>{
+  // receving properties from parent as an array of objects
+
+  const post=props.post;
+
     return(
         <View style={styles.container}>
           {/* Image */}
           <Image 
-            source={{uri:'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg'}}
+          source={{uri:post.image}}
            style={styles.image}/>
 
 
           {/* bed and bedroom */}
-             <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+    <Text style={styles.bedrooms}>{post.bedroom}</Text>
              <Text style={styles.description}
-             numberOfLines={3}>Suppose that you are a young child who lives in a small rural town. Every day as you go to school,
-                you watch as the buses pass. During summer break,
-                 you visit your aunt who lives in a major city.
-                  She takes you on a trip to the local railroad station so that you can watch the trains come and go.</Text> 
+             numberOfLines={3}>
+                {post.type}
+                  {''}
+                {post.title}</Text> 
          {/* old price & new price */}
           <Text style={styles.prices}>
-            <Text style={styles.oldPrice}>k250</Text>
-            <Text style={styles.newPrice}> k200</Text>
+            <Text style={styles.oldPrice}>{post.oldPrice}</Text>
+            <Text style={styles.newPrice}> {post.newPrice}</Text>
             / night
           </Text>
 
           {/*  Total price */}
-          <Text style={styles.totalPrice}>k450 total</Text>
+          <Text style={styles.totalPrice}>{post.totalPrice}</Text>
         </View>
     )
 }
