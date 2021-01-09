68ced8db2bd6fed1ced9c70afc2b0b6bc00cbd21
ginty1 <chiombagintykondwani@gmail.com>
2021-01-09 03:05:51 +0200

    search destinations styles

---
+++ scr/screens/DestinationSearch/styles.js
@@ -0,0 +1,30 @@
+import {StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+
+    container:{
+        margin:20,
+
+    },
+    textInpunt:{
+     fontSize:25,
+     color:'#000',
+     marginBottom:20,
+    },
+    row:{
+     flexDirection:'row',
+     alignItems:'center',
+     paddingVertical:16,
+     borderBottomWidth:1,
+     borderColor:'lightgrey'
+    },
+    iconContainer:{
+       backgroundColor:'#e7e7e7',
+       padding:10,
+       borderRadius:10,
+       marginRight:15,
+    },
+    description:{
+
+    }
+})
+export default styles;
